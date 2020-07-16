9d96ba132d82356e9327ef2edcb11e9cfe6a0912
wushanghui <wushanghui@aliyun.com>
2020-07-16 18:05:02 +0800

    commit

--- src/base/enums/EnumTest.java
+++ src/base/enums/EnumTest.java
@@ -13,16 +13,35 @@ public class EnumTest {
     public static void main(String[] args) {
         String input = "EXTRA_LARGE";
         Size size = Enum.valueOf(Size.class, input);
-        System.out.println("Size="+size);
+        System.out.println("Size=" + size);
         //缩写
-        System.out.println("abbreviation="+size.getAbbreviation());
-        System.out.println("size.ordinal()="+size.ordinal());
+        System.out.println("abbreviation=" + size.getAbbreviation());
+        System.out.println("size.ordinal()=" + size.ordinal());
+        Size small = Size.SMALL;
+        System.out.println(small.getSizeInfo()); // 我是一个尺寸枚举类
+        Size medium = Size.MEDIUM;
+        System.out.println(medium.getSizeInfo()); // 我是 M 号
     }
 }
 
-enum Size {
+interface SizeInfoInterface {
+    String getSizeInfo();
+}
 
-    SMALL("S"), MEDIUM("M"), LARGE("L"), EXTRA_LARGE("XL");
+enum Size implements SizeInfoInterface {
+
+    SMALL("S"),
+    MEDIUM("M") {
+        /**
+         * 若枚举值有不同的行为方式，可以自己实现接口的方法
+         */
+        @Override
+        public String getSizeInfo() {
+            return "我是 M 号";
+        }
+    },
+    LARGE("L"),
+    EXTRA_LARGE("XL");
 
     private String abbreviation;
 
@@ -32,5 +51,13 @@ enum Size {
 
     public String getAbbreviation() {
         return abbreviation;
+    }
+
+    /**
+     * 所有的枚举值都有相同的行为方式，可以这么实现接口的方法
+     */
+    @Override
+    public String getSizeInfo() {
+        return "我是一个尺寸枚举类";
     }
 }
