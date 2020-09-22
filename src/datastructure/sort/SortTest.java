4113db3409318464cecbf99b3da205a523bffa40
wushanghui <wushanghui@aliyun.com>
2020-09-22 11:35:09 +0800

    commit

--- src/datastructure/sort/SortTest.java
+++ src/datastructure/sort/SortTest.java
@@ -19,6 +19,21 @@ public class SortTest {
         // 算法2
         total = (start + end) * end / 2;
         System.out.println("算法2：" + total);
+        int n = 100;
 
+
+//        for (int i = 1; i <= n; i++) {
+//            int j = 1;
+//            while (j < n) {
+//                j = j * 2;
+//            }
+//        }
+
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
     }
 }
