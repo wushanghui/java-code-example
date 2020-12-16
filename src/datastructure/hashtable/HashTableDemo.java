050640e39b6bd9f809b367d7d13a961aaaf6ae26
wshcwj <wush93@aliyun.com>
2020-12-16 23:08:30 +0800

    commit

---
+++ src/datastructure/hashtable/HashTableDemo.java
@@ -0,0 +1,11 @@
+package datastructure.hashtable;
+
+/**
+ * @author jue
+ * @date 2020/12/16 22:39
+ * @describe
+ */
+public class HashTableDemo {
+
+
+}
--- src/datastructure/search/FibonacciSearch.java
+++ src/datastructure/search/FibonacciSearch.java
@@ -12,10 +12,13 @@ public class FibonacciSearch {
 
     public static void main(String[] args) {
         int[] arr = {1, 8, 10, 89, 1000, 1234};
+        int index = fibonacciSearch(arr, 1000);
+        System.out.println("index=" + index);
     }
 
     /**
      * 生成一个斐波那契数列
+     *
      * @return 斐波那契数列
      */
     public static int[] fibonacci() {
@@ -30,25 +33,46 @@ public class FibonacciSearch {
 
     /**
      * 斐波那契查找算法
+     *
      * @param arr 数组
      * @param key 要查找的值
      * @return key所在数组的小标，查不到为-1
      */
     public static int fibonacciSearch(int[] arr, int key) {
         int low = 0;
-        int high = arr.length-1;
+        int high = arr.length - 1;
         // 表示斐波那契分割数值的下标
-        int k =0;
-        int mid = 0;
+        int k = 0;
+        int mid;
         int[] f = fibonacci();
-        while (high > f[k] -1) {
+        System.out.println("斐波那契数列: " + Arrays.toString(f));
+        while (high > f[k] - 1) {
             k++;
         }
 
         int[] temp = Arrays.copyOf(arr, f[k]);
 
+        // temp = {1,8, 10, 89, 1000, 1234, 0, 0} => {1,8, 10, 89, 1000, 1234, 1234, 1234}
         for (int i = high + 1; i < temp.length; i++) {
-            temp[i]=arr[high]
+            temp[i] = arr[high];
         }
+
+        while (low <= high) {
+            mid = low + f[k - 1] - 1;
+            if (key < temp[mid]) {
+                high = mid - 1;
+                k--;
+            } else if (key > temp[mid]) {
+                low = mid + 1;
+                k -= 2;
+            } else {
+                if (mid <= high) {
+                    return mid;
+                } else {
+                    return high;
+                }
+            }
+        }
+        return -1;
     }
 }
