397bd8269a27f8a4386b08b098733f05f3c97cf0
wushanghui <wushanghui@aliyun.com>
2021-01-21 17:59:38 +0800

    code

--- src/base/thread/book/UserThreadTest.java
+++ src/base/thread/book/UserThreadTest.java
@@ -4,23 +4,20 @@ package base.thread.book;
  * @author wsh
  * @date 2021-01-20 14:23
  */
-public class DaemonThreadTest {
+public class UserThreadTest {
 
     public static void main(String[] args) {
 
-        Thread daemonThread = new Thread(new Runnable() {
+        Thread userThread = new Thread(new Runnable() {
             @Override
             public void run() {
                 for (;;) {}
             }
         });
 
-        // 设置为守护线程
-        daemonThread.setDaemon(true);
-        daemonThread.start();
+        userThread.start();
 
         System.out.println("main thread is over");
 
     }
-
 }
